4b2c80af9f646da2c13e8cfc0f7f37633d77a17b
Mohamed Ameer <mohamedammer.ali@ingka.com>
2024-02-19 14:49:14 +0100

    Schema creation

--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,51 @@
 import express from 'express';
-import { MongoClient } from 'mongodb';
-import { Client, Fleet, Ride } from './types';
-import { ObjectId } from 'mongodb';
+import mongoose from 'mongoose';
+import { ClientSchema, FleetSchema, BidSchema, RideSchema } from './model';
 const app = express();
 app.use(express.json());
 
-const client = new MongoClient('mongodb://localhost:27017');
-client.connect();
+mongoose.connect('mongodb+srv://admin:admin@cluster0.52ozvjb.mongodb.net/?retryWrites=true&w=majority');
 
-const db = client.db('taxiService');
-const ridesCollection = db.collection('rides');
-const bidsCollection = db.collection('bids');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+const ClientModel = mongoose.model('Client', ClientSchema);
+const FleetModel = mongoose.model('Fleet', FleetSchema);
+const BidModel = mongoose.model('Bid', BidSchema);
+const RideModel = mongoose.model('Ride', RideSchema);
 
 app.post('/rides', async (req, res) => {
-  const ride : Ride = req.body;
-  const result = await ridesCollection.insertOne(ride);
+  const ride = new RideModel(req.body);
+  const result = await ride.save();
   res.json(result);
 });
 
 app.get('/rides', async (req, res) => {
-  const rides = await ridesCollection.find().toArray();
+  const rides = await RideModel.find();
   res.json(rides);
 });
 
 app.post('/rides/:rideId/bids', async (req, res) => {
-  const bid = req.body;
-  bid.rideId = req.params.rideId;
-  const result = await bidsCollection.insertOne(bid);
+  const bid = new BidModel({ ...req.body, rideId: req.params.rideId });
+  const result = await bid.save();
   res.json(result);
 });
 
 app.get('/rides/:rideId/bids', async (req, res) => {
-  const bids = await bidsCollection.find({ rideId: req.params.rideId }).toArray();
+  const bids = await BidModel.find({ rideId: req.params.rideId });
   res.json(bids);
 });
 
 app.patch('/rides/:rideId/bids/:bidId', async (req, res) => {
-  const bidId = new ObjectId(req.params.bidId);
-  const result = await bidsCollection.updateOne({ _id: bidId }, { $set: { accepted: true } });
+  const result = await BidModel.updateOne({ _id: req.params.bidId }, { $set: { accepted: true } });
   res.json(result);
 });
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
 
 export default app;
